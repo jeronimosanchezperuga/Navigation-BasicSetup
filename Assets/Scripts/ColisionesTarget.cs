aa3c85c8dd3094023ec8ab757d2b811e2beaa439
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-09-13 09:57:01 -0300

    Initial commit

---
+++ Assets/Scripts/ColisionesTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionesTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {       
+        if (col.gameObject.CompareTag("Enemigo"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
